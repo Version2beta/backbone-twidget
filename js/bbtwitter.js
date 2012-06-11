ef3d7bcae9ca8b79eccd788aef7b53a16274723d
Rob Martin <rob@version2beta.com>
2012-06-11 00:03:45 -0500

    A basic starting point

---
+++ js/bbtwitter.js
@@ -0,0 +1,17 @@
+// bbtwitter.js
+// written by Rob Martin @version2beta and Arlen Walker @arlen
+// for demonstration of Backbone.js at the June 2012 @web414 meetup
+
+// Models
+
+
+// Collections
+
+
+// Views
+
+
+// Routers
+
+
+
